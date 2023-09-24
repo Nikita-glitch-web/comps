0857a94b932aa59af1a734c42ee1e570c127a072
Nick <nikitalytvynov0506@gmail.com>
2023-09-24 18:59:48 +0300

    24.09.23

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,23 @@
-function Table({ data }) {
-    const renderedRows = data.map((fruit) => {
+import { Fragment } from "react";
+
+function Table({ data, config, keyFn }) {
+    const renderedHeadres = config.map((column) => {
+        if (column.header) {
+           return <Fragment key={column.label}>{column.header()}</Fragment>;
+        }
+
+        return <th key={column.label}>{column.label}</th>
+    });
+
+
+
+    const renderedRows = data.map((rowData) => {
+        const renderedCells = config.map((column) => {
+           return <td className="p-2" key={column.label}>{rowData.render(fruit)}</td> 
+        });
         return (
-          <tr className="border-b" key={fruit.name}>
-            <td className="p-3">{fruit.name}</td>
-            <td className="p-3">
-                <div className={`p-3, m-2 ${fruit.color}`}></div>
-            </td>
-            <td className="p-3">{fruit.score}</td>
+          <tr className="border-b" key={keyFn(rowData)}>
+            {renderedCells}
           </tr>
         );
     })
@@ -14,9 +25,7 @@ function Table({ data }) {
       <table className="table-auto border-spacing-2">
         <thead>
           <tr className="border-b-2">
-            <th>Fruit</th>
-            <th>Color</th>
-            <th>Score</th>
+            {renderedHeadres}
           </tr>
         </thead>
         <tbody>{renderedRows}</tbody>
