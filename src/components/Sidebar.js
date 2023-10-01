af19644199ca5c85e4ea0c8c8aba054f3ba29fef
Nick <nikitalytvynov0506@gmail.com>
2023-10-01 18:22:30 +0300

    sii

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,7 +6,8 @@ function Sidebar() {
         { label: 'Accordion', path: '/accordion'},
         { label: 'Buttons', path: '/buttons' },
         { label: 'Modal', path: '/modal' },
-        { label: "Tabel", path: '/table'}
+        { label: "Tabel", path: '/table' },
+        { label: 'Counter', path: '/counter' }
     ];
 
     const renderedLinks = links.map((link) => {
